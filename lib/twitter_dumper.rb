e164ff0e0eb5ef958dc214a6c81acb2b7114548d
Fabio Akita <fabioakita@gmail.com>
2018-09-26 17:13:46 -0300

    changing to use the search feature of twitter and iterate day by day backwards

--- lib/twitter_dumper.rb
+++ lib/twitter_dumper.rb
@@ -3,50 +3,38 @@ require "selenium-webdriver"
 
 module TwitterDumper
   class Navigator
-    USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0)'
     attr_reader :url, :options, :driver
 
-    def initialize(username, shot_path = "/tmp")
-      @url = "https://twitter.com/#{username}"
-      @options = Selenium::WebDriver::Chrome::Options.new(args: ['headless', "user-agent=#{USER_AGENT}"])
+    def initialize(username, shot_path = "/tmp", since_date = '2013-01-01')
+      @since_date = Date.parse(since_date)
+      @url = "https://twitter.com/search?f=tweets&vertical=default&q=from%3A#{username}%20since%3A{since}%20until%3A{until}include%3Aretweet&src=typd"
+      @options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
       @driver = Selenium::WebDriver.for(:chrome, options: options)
       @shot_path = "#{shot_path}/#{username}_{counter}.png"
     end
 
     def pray_and_run!
-      initial_fetch!
       counter = 1
-      while true
-        puts "page #{counter} ..."
+      (@since_date..Date.today).each do |date|
+        url = @url.gsub("{since}", date.to_s).gsub("{until}", (date + 1).to_s)
+        puts "page #{counter} - #{url}"
+        @driver.get(url)
+        resize_window!
         current_shot(counter)
-        click_loader!
         counter += 1
       end
-    rescue Selenium::WebDriver::Error::NoSuchElementError
+    rescue
+      puts $!
+    ensure
       puts "probably done!"
     end
 
     private
 
-    def initial_fetch!
-      @driver.get(@url)
-      resize_window!
-    end
-
     def resize_window!
       width  = @driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
       height = @driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
       @driver.manage.window.resize_to(width, height)
-    end
-
-    def delete_tweets!
-      @driver.execute_script("var tweets = document.getElementsByClassName('tweet'); while(tweets[0]) { tweets[0].parentNode.removeChild(tweets[0])}")
-    end
-
-    def click_loader!
-      delete_tweets!
-      @driver.find_element(link_text: "Load older Tweets").click
-      resize_window!
     end
 
     def current_shot(counter = 0)
